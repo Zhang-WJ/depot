9cc57ecacd9b60ad71e47a13084621aa6971adc0
Zhangweijun <ahnqihyun@sina.cn>
2019-07-19 13:43:14 +0800

    ### New Updated
    购物车

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,12 @@
 class Cart < ApplicationRecord
     has_many :line_items, dependent: :destroy
+    before_destroy :ensure_not_referenced_by_any_line_item
+
+    private 
+        def ensure_not_referenced_by_any_line_item
+            unless line_items.empty?
+                errors.add(:base, 'Line Items present')
+                throw :abort
+        end
+    end
 end
